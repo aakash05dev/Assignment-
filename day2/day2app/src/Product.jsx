7cad440e3d2b80adec7829bba1f45b02eed95e12
aakash05dev <akashayyanar00@gmail.com>
2026-09-11 15:08:21 +0530

    day 2cCompleted

---
+++ day2/day2app/src/Product.jsx
@@ -0,0 +1,18 @@
+import React  from "react";
+
+function Product() {
+
+    return (
+    <div>
+        <h2>Product Details</h2>
+        <p>Name :  Laptop</p>
+        <p>Brand : Acer</p>
+        <p>Price : 45,000</p>
+        <p>Category : Elactronic</p>
+        <p>Stock : Available</p>
+
+    </div>
+    )
+}
+
+export default Product 
